57253301c9b681d7d9189d7cff83a69960fab8fd
Eduarda Machado Carreira <dumachado1755@gmail.com>
2023-11-14 20:30:35 -0300

    commit

--- GIT/JogoForca.Designer.cs
+++ GIT/JogoForca.Designer.cs
@@ -29,20 +29,94 @@ namespace GIT
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxLetra = new System.Windows.Forms.TextBox();
+            this.labelPalavraSecreta = new System.Windows.Forms.Label();
+            this.labelLetrasTentadas = new System.Windows.Forms.Label();
+            this.labelErrosRestantes = new System.Windows.Forms.Label();
+            this.buttonTentarLetra = new System.Windows.Forms.Button();
+            this.buttonReiniciar = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBoxLetra
+            // 
+            this.textBoxLetra.Location = new System.Drawing.Point(307, 93);
+            this.textBoxLetra.Name = "textBoxLetra";
+            this.textBoxLetra.Size = new System.Drawing.Size(113, 20);
+            this.textBoxLetra.TabIndex = 0;
+            this.textBoxLetra.TextChanged += new System.EventHandler(this.textBoxLetra_TextChanged);
+            // 
+            // labelPalavraSecreta
+            // 
+            this.labelPalavraSecreta.Location = new System.Drawing.Point(41, 218);
+            this.labelPalavraSecreta.Name = "labelPalavraSecreta";
+            this.labelPalavraSecreta.Size = new System.Drawing.Size(130, 36);
+            this.labelPalavraSecreta.TabIndex = 1;
+            this.labelPalavraSecreta.Text = "Palavra Secreta";
+            // 
+            // labelLetrasTentadas
+            // 
+            this.labelLetrasTentadas.Location = new System.Drawing.Point(41, 135);
+            this.labelLetrasTentadas.Name = "labelLetrasTentadas";
+            this.labelLetrasTentadas.Size = new System.Drawing.Size(130, 36);
+            this.labelLetrasTentadas.TabIndex = 2;
+            this.labelLetrasTentadas.Text = "Tentativas";
+            // 
+            // labelErrosRestantes
+            // 
+            this.labelErrosRestantes.Location = new System.Drawing.Point(41, 52);
+            this.labelErrosRestantes.Name = "labelErrosRestantes";
+            this.labelErrosRestantes.Size = new System.Drawing.Size(130, 36);
+            this.labelErrosRestantes.TabIndex = 3;
+            this.labelErrosRestantes.Text = "Erros Restantes";
+            // 
+            // buttonTentarLetra
+            // 
+            this.buttonTentarLetra.Location = new System.Drawing.Point(490, 93);
+            this.buttonTentarLetra.Name = "buttonTentarLetra";
+            this.buttonTentarLetra.Size = new System.Drawing.Size(96, 27);
+            this.buttonTentarLetra.TabIndex = 5;
+            this.buttonTentarLetra.Text = "Tentativa";
+            this.buttonTentarLetra.UseVisualStyleBackColor = true;
+            this.buttonTentarLetra.Click += new System.EventHandler(this.buttonTentarLetra_Click);
+            // 
+            // buttonReiniciar
+            // 
+            this.buttonReiniciar.Location = new System.Drawing.Point(637, 93);
+            this.buttonReiniciar.Name = "buttonReiniciar";
+            this.buttonReiniciar.Size = new System.Drawing.Size(96, 27);
+            this.buttonReiniciar.TabIndex = 6;
+            this.buttonReiniciar.Text = "Reiniciar";
+            this.buttonReiniciar.UseVisualStyleBackColor = true;
+            this.buttonReiniciar.Click += new System.EventHandler(this.buttonReiniciar_Click);
             // 
             // JogoForca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonReiniciar);
+            this.Controls.Add(this.buttonTentarLetra);
+            this.Controls.Add(this.labelErrosRestantes);
+            this.Controls.Add(this.labelLetrasTentadas);
+            this.Controls.Add(this.labelPalavraSecreta);
+            this.Controls.Add(this.textBoxLetra);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "JogoForca";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Jogo da Forca";
+            this.Load += new System.EventHandler(this.JogoForca_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxLetra;
+        private System.Windows.Forms.Label labelPalavraSecreta;
+        private System.Windows.Forms.Label labelLetrasTentadas;
+        private System.Windows.Forms.Label labelErrosRestantes;
+        private System.Windows.Forms.Button buttonTentarLetra;
+        private System.Windows.Forms.Button buttonReiniciar;
     }
 }
--- GIT/JogoForca.cs
+++ GIT/JogoForca.cs
@@ -12,9 +12,94 @@ namespace GIT
 {
     public partial class JogoForca : Form
     {
+
+        private void textBoxLetra_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private List<string> palavras = new List<string>
+        {
+            "ABACAXI", "ELEFANTE", "GIRASSOL", "PNEUMONIA", "PROGRAMACAO",
+            "CACHORRO", "GATO", "BANANA", "COMPUTADOR", "CARRO",
+            "MESA", "CADEIRA", "TELEFONE", "LIVRO", "CADERNO",
+            "PASTA", "CANETA", "JANELA", "PORTA", "COZINHA"
+        };
+
+        private List<char> letrasTentadas = new List<char>();
+        private string palavraSecreta;
+        private int errosRestantes;
+        private bool jogoEmAndamento = false;
+
         public JogoForca()
         {
             InitializeComponent();
+            ReiniciarJogo();
+        }
+
+        private void ReiniciarJogo()
+        {
+            Random rnd = new Random();
+            palavraSecreta = palavras[rnd.Next(palavras.Count)];
+            letrasTentadas.Clear();
+            errosRestantes = 6;
+            jogoEmAndamento = true;
+
+            AtualizarInterface();
+        }
+
+        private void AtualizarInterface()
+        {
+            labelPalavraSecreta.Text = string.Join(" ", palavraSecreta.Select(c => letrasTentadas.Contains(c) ? c : '_'));
+            labelLetrasTentadas.Text = string.Join(", ", letrasTentadas);
+
+            labelErrosRestantes.Text = $"Erros restantes: {errosRestantes}";
+
+            if (!jogoEmAndamento)
+            {
+                MessageBox.Show("Fim do jogo!");
+                ReiniciarJogo();
+            }
+        }
+
+        private void VerificarLetra(char letra)
+        {
+            if (!jogoEmAndamento || letrasTentadas.Contains(letra))
+                return;
+
+            letrasTentadas.Add(letra);
+
+            if (!palavraSecreta.Contains(letra))
+                errosRestantes--;
+
+            if (errosRestantes == 0 || palavraSecreta.All(c => letrasTentadas.Contains(c)))
+                jogoEmAndamento = false;
+
+            AtualizarInterface();
+        }
+
+        private void JogoForca_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTentarLetra_Click(object sender, EventArgs e)
+        {
+            if (textBoxLetra.Text.Length == 1)
+            {
+                char letra = textBoxLetra.Text.ToUpper()[0];
+                VerificarLetra(letra);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira apenas uma letra.");
+            }
+            textBoxLetra.Clear();
+        }
+
+        private void buttonReiniciar_Click(object sender, EventArgs e)
+        {
+            ReiniciarJogo();
         }
     }
 }
