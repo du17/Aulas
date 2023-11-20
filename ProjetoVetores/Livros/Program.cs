4f41211b1104af128afd8a6cd4a75545edac891f
du17 <95512620+du17@users.noreply.github.com>
2023-11-20 15:19:22 -0300

    Add files via upload

---
+++ ProjetoVetores/Livros/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cria um vetor para armazer até 9 referências para 
+            //objetos do tipo livro
+            Livro[] Estante = new Livro[9];
+
+            //ERRO:  System.NullReferenceException
+            //string TituloDoLivro = Estante[4].Titulo;
+
+            //cria um objeto do tipo Livro e guarda a referência dele na
+            //quinta posição (índice 4) do vetor
+            Estante[4] = new Livro("Moby Dick", "Herman Melville");
+
+            //recupera a referência para um objeto do tipo livro e acessa
+            //o valor do campo "Titulo"
+            string TituloDoLivro = Estante[4].Titulo;
+            Console.WriteLine("Título: {0}", TituloDoLivro);
+
+            //Percorre o vetor de Livros
+            Console.WriteLine("\nLivros: ");
+            for(int i = 0; i < Estante.Length; i++)
+            {
+                //verifica se a posição do vetor contém uma referência válida
+                //para um objeto do tipo Livro
+                if(Estante[i] != null)
+                {
+                    //imprime o título do livro na tela
+                    Console.WriteLine("* {0}", Estante[i].Titulo);
+                } 
+            }
+        }
+    }
+}
---
+++ ProjetoVetores/Matriz/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // cria e inicializa uma matriz 4 X 6
+            /*int[,] itens = { {  1,  2,  3,  4 },
+                                {  5,  6,  7,  8 },
+                                {  9, 10, 11, 12 },
+                                { 13, 14, 15, 16 },
+                                { 17, 18, 19, 20 } };*/
+
+            // cria  uma matriz 6 X 4 (6 linhas e 4 colunas)
+            int[,] itens = new int[6, 4];
+
+            itens[2, 3] = 99; // o elemento na posição (2,3) recebe “99”
+
+            itens[2, 2] = itens[2, 3]; //copia o elemento (2,3) para o (2,2)
+
+            itens[2, 3]++; // incrementa o valor do elemento (2,3)
+
+            //Percorre todos os elementos da matriz
+            for (int i = 0; i < itens.GetLength(0); i++)
+            {
+                for(int j = 0; j < itens.GetLength(1); j++)
+                {
+                    Console.Write("{0}\t", itens[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
