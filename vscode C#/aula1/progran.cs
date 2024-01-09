9df5551f95fd696b0783dc635ea43fd000c22961
du17 <95512620+du17@users.noreply.github.com>
2024-01-08 21:06:18 -0300

    Add files via upload

---
+++ vscode C#/aula1/progran.cs
@@ -0,0 +1,45 @@
+// dia 08/01
+
+using System;
+
+public class HelloWord{
+
+    public static void Main(){
+
+        /*string nome = "gabriel";
+        string sobrenome = " mesquita";
+        //string complemento = nome + sobrenome; tambem funciona
+        string usuarioNome;
+        string usuarioSobrenome;
+        int idade = 28;
+
+        Console.WriteLine(nome + sobrenome);
+        Console.WriteLine(idade);
+
+        Console.WriteLine("\nDigite seu nome: ");
+        usuarioNome = Console.ReadLine();
+
+        Console.WriteLine("\nsobrenome: ");
+        usuarioSobrenome = Console.ReadLine();
+
+        Console.WriteLine(usuarioNome + usuarioSobrenome);*/
+
+        /*string[] carros = {"volvo", "bmw", "ford", "mazda"};
+
+        for(int i = 0; i < carros.Length; i++){
+            
+            Console.WriteLine(carros[i]);
+        }*/
+
+        //Console.WriteLine(carros.Length); -> tamanho do vetor
+
+        var paises = new List<string>();
+
+        paises.add("brasil");
+
+
+
+
+    }
+
+}
